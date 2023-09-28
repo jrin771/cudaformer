bcbb0609e14c0884fc2b5ec0b8931d5b0a45e6b2
Jacob Rintamaki <jrin@stanford.edu>
2023-09-28 13:03:40 -0700

    Create FFNN.cu

---
+++ FFNN.cu
@@ -0,0 +1,5 @@
+//cuda code for my feedforward neural network  
+//literally just use this: https://luniak.io/cuda-neural-network-implementation-part-1/#programming-model (LMAO) 
+//but I can modify it to be like the cross entropy for language modeling stuff anyways.  
+
+//also I think I could make it like one kernel or one file and such and that could be much more useful and FUN anyways for all of this. 
